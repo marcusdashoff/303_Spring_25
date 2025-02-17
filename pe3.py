7d6b80598c2809d3017a9858385a97a8d1320ace
Chen Zhu <patrickzhu1130@gmail.com>
2025-02-16 23:13:58 -0800

    Patch bankaccount class

--- pe3.py
+++ pe3.py
@@ -1,4 +1,5 @@
 import string
+import datetime
 
 def encode(input_text, shift):
     begin_position = ord('a')
@@ -15,12 +16,36 @@ def decode(input_text, shift):
     return encode(input_text, -shift)[1]
 
 class BankAccount:
-    name = "Rainy"
-    id = "1234"
-    created_date = datetime.date.today()
-    balance: int = 0
+    def __init__(self, name = 'Rainy', id = '1234', created_date = datetime.date.today(), balance = 0):
+        if not isinstance(created_date, datetime.date): 
+            raise TypeError('wrong date type') 
+        if created_date > datetime.date.today():
+            raise ValueError('creation date cannot be futyre date of today') 
+        self.name = name
+        self.id = id
+        self.created_date = created_date
+        self.balance = balance
+        
     def deposit(self, amount):
-        self.balance += amount
+        if amount > 0: 
+            self.balance += amount
+        print(self.balance)
+        
     def withdraw(self, amount):
+        if amount > 0:
+            self.balance -= amount
+        print(self.balance)
+        
     def view_balance(self):
         return self.balance
+    
+class SavingsAccount(BankAccount):
+    def withdraw(self, amount):
+        return super().withdraw(amount) if self.balance - amount >= 0 and (datetime.date.today() - self.created_date).days >= 180 else None
+
+class CheckingAccount(BankAccount):
+    def withdraw(self, amount):
+        if self.balance - amount < 0:
+            amount += 30 
+        return super().withdraw(amount)
+    
